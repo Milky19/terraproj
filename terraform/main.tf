aaf740cb8e978c438c87fbf4e101c458971f4dd0
root <root@ip-172-31-9-140.ec2.internal>
2022-12-01 09:45:17 +0000

    terraproject

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region       = "us-east-1"
+  access_key   = "AKIAWO5LHJ53K7HQ2HDO"
+  secret_key   = "8h6fQWIkcG4kHOoETkS5E7FheAhPJpsecWjPe+Jl"
+}
+
+resource "aws_instance" "example" {
+  ami        = "ami-02b972fec07f1e659"
+  instance_type = "t2.micro"
+  
+   tags  = {
+     Name = "server"
+}
+}
+
+
