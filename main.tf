43a059a497789c916c824d6fd9a241a07137eee2
Milky19 <97377003+Milky19@users.noreply.github.com>
2022-12-01 02:22:33 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,6 @@ provider "aws" {
   secret_key   = "8h6fQWIkcG4kHOoETkS5E7FheAhPJpsecWjPe+Jl"
 }
 
-resource "aws_instance" "example" {
-  ami        = "ami-02b972fec07f1e659"
-  instance_type = "t2.micro"
-  
-   tags  = {
-     Name = "server"
-}
+resource "aws_s3_bucket" "one" {
+  bucket = "krishna906"
 }
